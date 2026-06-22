2fe5629a52667510bc77d673f421e793a553886d
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:36:26 +0530

    [ProductionAI] Add infra/modules/loadbalancer/main.tf

--- infra/modules/loadbalancer/main.tf
+++ infra/modules/loadbalancer/main.tf
@@ -1,20 +1,20 @@
-resource "aws_lb" "app" {
-  name               = "app-alb"
+resource "aws_lb" "main" {
+  name               = "main-alb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb.id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = module.networking.subnet_ids
 
   enable_deletion_protection = true
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.app.arn
+  load_balancer_arn = aws_lb.main.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
+    type             = "redirect"
     redirect {
       protocol = "HTTPS"
       port     = "443"
@@ -24,7 +24,7 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.app.arn
+  load_balancer_arn = aws_lb.main.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
@@ -32,13 +32,31 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
+    target_group_arn = aws_lb_target_group.main.arn
   }
 }
 
-resource "aws_lb_target_group" "app" {
-  name     = "app-target-group"
-  port     = var.app_port
-  protocol = "HTTP"
-  vpc_id   = module.networking.vpc_id
+resource "aws_security_group" "alb_sg" {
+  vpc_id = module.networking.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
