bd9b91238a3bf0e9ca7ded16fca5e3ababed39f8
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:38:06 +0530

    [ProductionAI] Add infra/modules/loadbalancer/main.tf

--- infra/modules/loadbalancer/main.tf
+++ infra/modules/loadbalancer/main.tf
@@ -1,62 +1,48 @@
-resource "aws_lb" "main" {
-  name               = "main-alb"
+
+resource "aws_lb" "app" {
+  name               = "app-lb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = module.networking.subnet_ids
+  subnets            = var.alb_subnets
 
   enable_deletion_protection = true
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.main.arn
+  load_balancer_arn = aws_lb.app.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "redirect"
+    type = "redirect"
+
     redirect {
-      protocol = "HTTPS"
-      port     = "443"
+      port        = "443"
+      protocol    = "HTTPS"
       status_code = "HTTP_301"
     }
   }
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
+  load_balancer_arn = aws_lb.app.arn
   port              = 443
   protocol          = "HTTPS"
+
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.certificate_arn
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.main.arn
+
+    target_group_arn = aws_lb_target_group.app.arn
   }
 }
 
-resource "aws_security_group" "alb_sg" {
-  vpc_id = module.networking.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_lb_target_group" "app" {
+  name     = "app-targets"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
 }
