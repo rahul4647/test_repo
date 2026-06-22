b56a7721da9ac977c0082ab1051de8fd47a7d6d4
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 23:36:08 +0530

    [ProductionAI] Add infra/modules/loadbalancer/main.tf

---
+++ infra/modules/loadbalancer/main.tf
@@ -0,0 +1,47 @@
+resource "aws_lb" "main" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [module.networking.alb_sg_id]
+  subnets            = module.networking.public_subnets
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "redirect"
+    redirect {
+      protocol = "HTTPS"
+      port     = "443"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = "arn:aws:acm:<region>:<account-id>:certificate/<certificate-id>"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "app-target-group"
+  port     = 3000
+  protocol = "HTTP"
+  vpc_id   = module.networking.vpc_id
+  health_check {
+    path                = "/health"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
