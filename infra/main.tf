86b5143622f8b1853a431c70804d0688f47d3993
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:38:40 +0530

    [ProductionAI] Add infra/main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = var.region
+  region  = var.region
   version = "~> 5.0"
 }
 
@@ -16,13 +16,14 @@ module "compute" {
 module "database" {
   source = "./modules/database"
   vpc_id = module.networking.vpc_id
-  db_sg_id = module.networking.db_sg_id
+  db_subnet_group_name = module.networking.db_subnet_group_name
 }
 
 module "loadbalancer" {
   source = "./modules/loadbalancer"
   vpc_id = module.networking.vpc_id
   alb_sg_id = module.networking.alb_sg_id
+  alb_target_group_arn = module.compute.alb_target_group_arn
 }
 
 module "iam" {
