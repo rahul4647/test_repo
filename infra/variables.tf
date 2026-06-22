5a5f948abc480a514f2cad6595e360bac2610abd
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 20:02:51 +0530

    [ProductionAI] Add infra/variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,25 +1,13 @@
 # Input variables
-variable "aws_region" {
+variable "app_port" {
+  type = number
+  default = 3000
+}
+variable "db_port" {
+  type = number
+  default = 27017
+}
+variable "db_engine" {
   type = string
-  default = "us-east-1"
-}
-
-variable "vpc_cidr" {
-  type = string
-  default = "10.0.0.0/16"
-}
-
-variable "availability_zones" {
-  type = list(string)
-  default = ["us-east-1a", "us-east-1b"]
-}
-
-variable "public_subnets" {
-  type = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnets" {
-  type = list(string)
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+  default = "documentdb"
 }
