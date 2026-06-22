c70faab78ec6eb5aaa757b3f5078ab1135e264a4
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 20:03:01 +0530

    [ProductionAI] Add infra/modules/secrets/main.tf

--- infra/modules/secrets/main.tf
+++ infra/modules/secrets/main.tf
@@ -1,10 +1,12 @@
-# Create a secrets manager secret
+# Create secrets
 resource "aws_secretsmanager_secret" "this" {
-  name = var.name
+  name = "my-secret"
 }
 
-# Create a secrets manager secret version
 resource "aws_secretsmanager_secret_version" "this" {
   secret_id = aws_secretsmanager_secret.this.id
-  secret_string = jsonencode(var.secret)
+  secret_string = jsonencode({
+    NEXT_CLERK_WEBHOOK_SECRET = "my-next-clerk-webhook-secret"
+    MONGODB_URL = "my-mongodb-url"
+  })
 }
