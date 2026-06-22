00b765573b9395da5aede0a11d0b8d0c2610b052
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 20:02:56 +0530

    [ProductionAI] Add infra/modules/compute/main.tf

--- infra/modules/compute/main.tf
+++ infra/modules/compute/main.tf
@@ -1,24 +1,24 @@
 # Create an ECS cluster
 resource "aws_ecs_cluster" "this" {
-  name = var.cluster_name
+  name = "my-ecs-cluster"
 }
 
 # Create an ECS task definition
 resource "aws_ecs_task_definition" "this" {
-  family = "my-task-definition"
-  cpu = var.cpu
-  memory = var.memory
-  network_mode = "awsvpc"
+  family = "my-ecs-task"
   requires_compatibilities = ["FARGATE"]
-  execution_role_arn = aws_iam_role.this.arn
+  network_mode = "awsvpc"
+  cpu = 1024
+  memory = 2048
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
   container_definitions = jsonencode([
     {
       name = "my-container"
-      image = "my-image"
+      image = "my-docker-image"
       portMappings = [
         {
-          containerPort = 3000
-          hostPort = 3000
+          containerPort = var.app_port
+          hostPort = var.app_port
           protocol = "tcp"
         }
       ]
@@ -26,18 +26,52 @@ resource "aws_ecs_task_definition" "this" {
   ])
 }
 
-# Create an IAM role
-resource "aws_iam_role" "this" {
-  name = "my-iam-role"
+# Create an ECS service
+resource "aws_ecs_service" "this" {
+  name = "my-ecs-service"
+  cluster = aws_ecs_cluster.this.name
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count = 2
+  launch_type = "FARGATE"
+  network_configuration {
+    subnets = [aws_subnet.public[0].id]
+    security_groups = [aws_security_group.ecs.id]
+    assign_public_ip = "ENABLED"
+  }
+}
+
+# Create a security group for ECS
+resource "aws_security_group" "ecs" {
+  vpc_id = var.vpc_id
+  egress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 27017
+    to_port = 27017
+    protocol = "tcp"
+    cidr_blocks = [aws_security_group.documentdb.cidr_blocks[0]]
+  }
+  tags = {
+    Name = "my-ecs-sg"
+  }
+}
+
+# Create IAM role for ECS task execution
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "my-ecs-task-execution"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Action = "sts:AssumeRole"
+        Effect = "Allow"
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
         }
-        Effect = "Allow"
       }
     ]
   })
