854990bd8854d666913a1487daf64e44b0d0d4d9
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 22:35:41 +0530

    [ProductionAI] Add infra/modules/iam/main.tf

---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,38 @@
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  ]
+
+  inline_policy {
+    name = "ScopedLogPolicy"
+
+    policy = jsonencode({
+      Version = "2012-10-17",
+      Statement = [
+        {
+          Effect = "Allow",
+          Action = [
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          Resource = "arn:aws:logs:*:*:log-group:/ecs/app:*"
+        }
+      ]
+    })
+  }
+}
