c0764ad684bb7a139b772c2f68ccb3d999693593
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:37:59 +0530

    [ProductionAI] Add infra/outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,14 +1,20 @@
+
 output "vpc_id" {
-  description = "The ID of the VPC."
+  description = "The VPC ID."
   value       = module.networking.vpc_id
 }
 
 output "alb_dns_name" {
-  description = "The DNS name of the load balancer."
+  description = "The DNS name of the ALB."
   value       = module.loadbalancer.alb_dns_name
 }
 
-output "db_endpoint" {
-  description = "The connection endpoint for the DocumentDB cluster."
-  value       = module.database.endpoint
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster."
+  value       = module.compute.ecs_cluster_name
+}
+
+output "db_instance_endpoint" {
+  description = "The endpoint of the DocumentDB instance."
+  value       = module.database.db_instance_endpoint
 }
