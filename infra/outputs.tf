f101ab3ad1fb805df5319933fa1e26ccea4c7f73
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 20:04:49 +0530

    [ProductionAI] Add infra/outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,15 @@
-# Output values
 output "vpc_id" {
   value = module.vpc.vpc_id
 }
-output "ecs_cluster_name" {
-  value = module.ecs.ecs_cluster_name
+output "subnet_ids" {
+  value = module.vpc.subnet_ids
+}
+output "ecs_cluster_id" {
+  value = module.ecs.ecs_cluster_id
 }
 output "documentdb_instance_id" {
   value = module.documentdb.documentdb_instance_id
 }
-output "alb_dns_name" {
-  value = module.alb.alb_dns_name
+output "alb_id" {
+  value = module.alb.alb_id
 }
