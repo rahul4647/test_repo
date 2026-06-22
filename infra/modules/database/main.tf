9a83ab0f5059843d150ac10e4f9988956879c9ce
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:38:04 +0530

    [ProductionAI] Add infra/modules/database/main.tf

--- infra/modules/database/main.tf
+++ infra/modules/database/main.tf
@@ -1,31 +1,18 @@
+
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier      = "docdb-cluster"
-  engine_version          = "4.0.0"
-  master_username         = "admin"
-  master_password         = "${var.master_password}"
+  cluster_identifier      = "main-docdb"
+  master_username         = "docdbuser"
+  master_password         = "${var.mongodb_url}"
   backup_retention_period = 7
   preferred_backup_window = "07:00-09:00"
+  vpc_security_group_ids  = [aws_security_group.db_sg.id]
   storage_encrypted       = true
   deletion_protection     = true
-  skip_final_snapshot     = false
-
-  vpc_security_group_ids = [aws_security_group.db_sg.id]
 }
 
 resource "aws_docdb_cluster_instance" "main" {
-  count              = 1
-  identifier         = "docdb-instance-${count.index}"
-  cluster_identifier = aws_docdb_cluster.main.id
-  instance_class     = var.db_instance_class
-}
-
-resource "aws_security_group" "db_sg" {
-  vpc_id = module.networking.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = []
-  }
+  identifier          = "main-docdb-instance"
+  cluster_identifier  = aws_docdb_cluster.main.id
+  instance_class      = "db.t3.micro"
+  apply_immediately   = true
 }
