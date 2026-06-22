a26e798003c27803287925a86b59446cc40c4307
rahul4647 <148080301+rahul4647@users.noreply.github.com>
2026-06-22 19:38:50 +0530

    [ProductionAI] Add infra/modules/loadbalancer/main.tf

--- infra/modules/loadbalancer/main.tf
+++ infra/modules/loadbalancer/main.tf
@@ -1,20 +1,20 @@
 resource "aws_lb" "main" {
-  name               = "${var.app_name}-alb"
+  name               = "app-loadbalancer"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = module.networking.subnet_ids
+  security_groups    = [var.alb_sg_id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = true
 
   tags = {
-    Name = "${var.app_name}-alb"
+    Name = "app-lb"
   }
 }
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
 
   default_action {
@@ -30,7 +30,7 @@ resource "aws_lb_listener" "http" {
 
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.main.arn
-  port              = "443"
+  port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.certificate_arn
@@ -42,20 +42,33 @@ resource "aws_lb_listener" "https" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${var.app_name}-tg"
+  name     = "app-target-group"
   port     = 3000
   protocol = "HTTP"
-  vpc_id   = module.networking.vpc_id
+  vpc_id   = var.vpc_id
 
   health_check {
     path                = "/health"
+    protocol            = "HTTP"
     interval            = 30
     timeout             = 5
-    healthy_threshold   = 5
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 
   tags = {
-    Name = "${var.app_name}-tg"
+    Name = "app-tg"
   }
+}
+
+variable "alb_sg_id" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "certificate_arn" {
+  type = string
 }
